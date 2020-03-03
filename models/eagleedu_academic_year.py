f33333b1128eb4d30c502e5a549996a967a17a41
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2020-03-04 02:35:11 +0600

    trying add first day and last day of the year

--- models/eagleedu_academic_year.py
+++ models/eagleedu_academic_year.py
@@ -1,7 +1,16 @@
 from eagle import fields, models, api, _
 from eagle.exceptions import ValidationError
-from datetime import datetime,date
+# from datetime import datetime timedelta
 from calendar import monthrange
+
+from datetime import datetime, timedelta
+
+_defaults={
+          'date_start': lambda *a:datetime.now().strftime('%Y-%m-%d'),
+          'date_end': lambda *a:(datetime.now() + timedelta(days=(6))).strftime('%Y-%m-%d'),
+}
+
+
 
 # class AcademyRoomNo(models.Model):
 #     _name='education.rooms'
@@ -17,13 +26,37 @@ class EagleeduAcademicYear(models.Model):
     _rec_name = 'name'
 
     name = fields.Char(string='Year Name', required=True, help='Name of academic year')
-    ay_code = fields.Char(string='Code', required=True, help='Code of academic year')
+    # academic_year_id = fields.Char(string='Year Name', required=True, help='Name of academic year')
+    ay_code = fields.Char(string='Code', related='name', help='Code of academic year')
     sequence = fields.Integer(string='Sequence', required=True)
-    academic_year_start_date = fields.Date(string='Start date', required=True, help='Starting date of academic year')
-    academic_year_end_date = fields.Date(string='End date', required=True, help='Ending of academic year')
+
+    current_year = datetime.now().year
+
+    start_date = fields.Datetime(string='Start Date', default=datetime.strptime('%s-01-01' % (current_year), '%Y-%m-%d'))
+    end_date = fields.Datetime(string='End Date', default=datetime.strptime('%s-12-31' % (current_year), '%Y-%m-%d'))
+
+
+    # academic_year_start_date = fields.Date(string='Start date', required=True, help='Starting date of academic year')
+    # academic_year_end_date = fields.Date(string='End date', required=True, help='Ending of academic year')
+
+    # start_date = fields.Date(string='Start date', required=True, help='Starting date of academic year')
+    # end_date = fields.Date(string='End date', required=True, help='Ending of academic year')
+
+    # start_date = fields.Date(string='Start Date', required=True, default=datetime.date.strftime('%Y-01-01'))
+
+    # starting_day_of_current_year = datetime.now().date().replace(month=1, day=1)
+    # ending_day_of_current_year = datetime.now().date().replace(month=12, day=31)
+
+    # start_date = fields.Date(string='Start date', required=True, default=time.strftime('%Y-01-01'))
+    # end_date = fields.Date(string='End date', required=True, default=time.strftime('%Y-31-12'))
+
+    # date_to = fields.Date(string="End Date", default=time.strftime('%Y-01-01'))
+
+
     academic_year_description = fields.Text(string='Description', help="Description about the academic year")
     active = fields.Boolean('Active', default=True,
                             help="If unchecked, it will allow you to hide the Academic Year without removing it.")
+
 
     @api.model
     def create(self, vals):
@@ -32,3 +65,8 @@ class EagleeduAcademicYear(models.Model):
         vals['sequence'] = self.env['ir.sequence'].next_by_code('eagleedu.academic.year')
         res = super(EagleeduAcademicYear, self).create(vals)
         return res
+
+    # @api.onchange('academic_year_id')
+    # def set_ay_code(self):
+    #     if self.academic_year_id:
+    #         self.ay_code = self.academic_year_id.name
--- models/eagleedu_class.py
+++ models/eagleedu_class.py
@@ -9,7 +9,7 @@ class EagleeduClass(models.Model):
     _description = "Class Standard Level"
     sequence=fields.Integer("Sequence")
     name = fields.Char(string='Class', required=True, help="Enter the Name of the Class")
-    code = fields.Char(string='Code', required=True, help="Enter the Code of the Class")
+    code = fields.Char(string='Code', related='name', help="Enter the Code of the Class")
     class_category = fields.Many2one('eagleedu.class.category', string='Class Category', help="Enter the Name of the Class")
     class_id = fields.Many2one('eagleedu.class', string='Class', help="Enter the Name of the Class")
 
