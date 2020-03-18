13079f5e58ac89e793adbff692aa778938d08a2a
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2020-03-19 02:49:48 +0600

    trying to set multi selection field class category and class name

--- models/eagleedu_register.py
+++ models/eagleedu_register.py
@@ -7,7 +7,8 @@ class EagleeduRegister(models.Model):
     _description='this admission register book'
     name=fields.Char('Register Book Name', compute='get_name')
     class_category=fields.Many2one('eagleedu.class.category',required=True, track_visibility='onchange')
-    standard=fields.Many2one('eagleedu.class', required=True,)
+    standard=fields.Many2one('eagleedu.class', required=True)
+    # class_id=fields.Many2one('eagleedu.class', related='class_category.class_id')
     academic_year=fields.Many2one('eagleedu.academic.year', "For the year" , required=True)
     start_time = fields.Datetime(string='Application Starts on',default=lambda self: fields.datetime.now())
     end_time = fields.Datetime(string='Application ends on',default=lambda self: fields.datetime.now())
@@ -22,6 +23,43 @@ class EagleeduRegister(models.Model):
         for rec in self:
             if rec.class_category and rec.standard and rec.academic_year:
                 rec.name=rec.class_category.name + rec.standard.name +'-'+rec.academic_year.name+' '+'Admission'
+
+
+
+    #
+    # partner_id = fields.Many2one('res.parner', string="Partner")
+    #
+    # phone = fields.Char(string="Identification", related='partner_id .phone')
+    #
+
+    # @api.onchange('class_category')
+    # def set_class(self):
+    #     for rec in self:
+    #         self.standard==self.class_category
+
+            #
+            # if rec.class_category:
+            #     rec.standard=rec.class_category.standard
+
+
+    # @api.onchange('partner_id')
+    # def on_change_field(self):
+    #     self.phone = self.partner_id.phone
+
+    # @api.onchange('class_category')
+    # def set_class(self):
+    #     for rec in self:
+    #         if rec.class_category:
+    #             rec.standard=rec.class_category.standard
+    #
+    # PACKAGE_TYPE_SELECTION = []
+    #
+    # class test(osv.osv):
+    #     _name = "Test"
+    #
+    # _columns = {
+    #     'selection': fields.selection(PACKAGE_TYPE_SELECTION, string='Selection'),
+    # }
 
     # @api.onchange ('class_category', 'standard')
     # def get_name(self):
